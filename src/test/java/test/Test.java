0a8943845be68334d9a0258519af1fd042ccc931
Thibault <thibault.dvd@outlook.fr>
2021-08-19 10:14:48 +0200

    Test Nico

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -9,19 +9,12 @@ public class Test {
 
 	public static void main(String[] args) {
 						
-		EntityManager em = Context.getInstance().getEmf().createEntityManager();
-		
-		Test_Nico t = new Test_Nico(10);
-		
+		/*EntityManager em = Context.getInstance().getEmf().createEntityManager();
 		em.getTransaction().begin();
-		
 		em.persist(t);
-		
 		em.getTransaction().commit();
-		
 		em.close();
-				
-		Context.getInstance().closeEmf();
+		Context.getInstance().closeEmf();*/
 		
 	}
 
