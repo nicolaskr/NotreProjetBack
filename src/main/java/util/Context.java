131cbaabe541f9232d69b07c58e5c12982a407cf
Thibault <thibault.dvd@outlook.fr>
2021-08-19 11:37:23 +0200

    Context

--- src/main/java/util/Context.java
+++ src/main/java/util/Context.java
@@ -3,9 +3,18 @@ package util;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dao.DAOBatiment;
+import dao.DAOCompte;
+import dao.DAOPartie;
+import dao.DAORessource;
+
 public class Context {
 
 	private static Context _instance=null;
+	private static DAOBatiment daoB;
+	private static DAOCompte daoC;
+	private static DAOPartie daoP;
+	private static DAORessource daoR;
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("NotreProjetBack");
 		
 	private Context() {}	
@@ -19,6 +28,21 @@ public class Context {
 		return emf;
 	}
 
+	public DAOBatiment getDaoB() {
+		return daoB;
+	}
+	
+	public DAOCompte getDaoC() {
+		return daoC;
+	}
+	
+	public DAOPartie getDaoP() {
+		return daoP;
+	}
+	
+	public DAORessource getDaoR() {
+		return daoR;
+	}
 	
 	public void closeEmf() {
 		emf.close();
