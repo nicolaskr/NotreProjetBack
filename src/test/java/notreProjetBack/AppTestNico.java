4538ac7232342eddc18564e9b21f790953d7d9f9
NicoK <nicolas.kroupa@gmail.com>
2021-08-26 13:36:58 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/test/java/notreProjetBack/AppTestNico.java
@@ -0,0 +1,16 @@
+package test;
+
+import notreProjetBack.AppSpringNico;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import notreProjetBack.config.AppConfig;
+
+public class AppTestNico {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        ctx.getBeanFactory().createBean(AppSpringNico.class).run(args);
+        ctx.close();
+    }
+
+}
