a4233e33fd9d05e25e1fe84900bc6aacc36b9066
Johan <johan.loison@yahoo.com>
2021-08-27 09:15:47 +0200

    Service

---
+++ src/main/java/notreProjetBack/services/DraftService.java
@@ -0,0 +1,114 @@
+package notreProjetBack.services;
+
+import java.util.List;
+
+import model.Ressource;
+
+public class DraftService {
+	
+	//-------------------------------------------------------------------------------------------------
+	//Batiment=========================================================================================
+	//--------------------------------------------------------------------------------------------------
+	public String toStringWithCost() {	
+		
+		return "";
+	}
+	
+	//Attaque
+	public void upgrade() {}
+	
+	//Defense
+	public void upgrade() {}
+	
+	//Bucheron
+	public void upgrade() {}
+
+	//Carrier
+	public void upgrade() {}
+
+	//Carriere
+	public void upgrade() {}
+
+	//Catapulte
+	public void upgrade() {}
+	
+	//Fonderie
+	public void transformation (String r1, int nb,  List <Ressource> re){}
+	
+	//Forteresse
+	public void upgrade() {}
+	
+	//Four
+	public void transformation (String r1, int nb,  List <Ressource> re){}
+	
+	//Merveille
+	public void upgrade() {}
+	
+	//Mine
+	public void upgrade() {}
+	
+	//Mineur
+	public void upgrade() {}
+	
+	//Muraille
+	public void upgrade() {}
+
+	
+	
+	//-------------------------------------------------------------------------------------------------
+	//Ressource=========================================================================================
+	//--------------------------------------------------------------------------------------------------	
+	
+	//Charbon
+	public void actuAchat(List<Ressource> cost) {}
+	
+	public void actuGain (int gain) {}
+	
+	//Cuivre
+	public void actuAchat(List<Ressource> cost) {}
+		
+	public void actuGain (int gain) {}
+	
+	//Fer
+	public void actuAchat(List<Ressource> cost) {}
+			
+	public void actuGain (int gain) {}
+	
+	//Or
+	public void actuAchat(List<Ressource> cost) {}
+			
+	public void actuGain (int gain) {}
+	
+	//Or
+	public void actuAchat(List<Ressource> cost) {}
+				
+	public void actuGain (int gain) {}
+		
+	//Or
+	public void actuAchat(List<Ressource> cost) {}
+			
+	public void actuGain (int gain) {}
+		
+	//Or
+	public void actuAchat(List<Ressource> cost) {}
+				
+	public void actuGain (int gain) {}
+	
+	//-------------------------------------------------------------------------------------------------
+	//Compte=========================================================================================
+	//--------------------------------------------------------------------------------------------------
+	
+	//Joueur
+	
+	//Admin
+	
+	//
+	
+	//-------------------------------------------------------------------------------------------------
+	//Partie=========================================================================================
+	//--------------------------------------------------------------------------------------------------
+	
+	
+	
+
+}
