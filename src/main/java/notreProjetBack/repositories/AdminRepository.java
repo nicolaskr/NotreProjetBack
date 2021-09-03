5611f4d331b37839d7a401e736da28f03d5793f8
Pierre <pi.lequay@gmail.com>
2021-09-03 11:44:32 +0200

    CompteService

---
+++ src/main/java/notreProjetBack/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package notreProjetBack.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import notreProjetBack.model.Admin;
+
+public interface AdminRepository extends JpaRepository <Admin, Integer>{
+
+}
